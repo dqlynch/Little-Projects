cb680d58b1869a136ca8d09cbec3d04536fbd909
dqlynch <dqlynch@umich.edu>
2016-04-07 13:13:12 -0400

    isolated sieve fn for reuse

--- sieve_of_eratosthenes.cpp
+++ sieve_of_eratosthenes.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 // REQUIRES:    n >= 2
 // EFFECTS:     finds and prints primes up to n
-void find_primes(const long long max);
+bool* sieve_of_e(const long long max);
 
 int main () {
     // Ask for a number
@@ -20,11 +20,6 @@ int main () {
     cout << "Find primes up to: ";
     cin >> n;
 
-    // Find primes up to that number
-    find_primes(n);
-}
-
-void find_primes(const long long max) {
 
     // Ask if user wants to print primes
     bool print_request = false;
@@ -44,6 +39,39 @@ void find_primes(const long long max) {
     // Start timer
     clock_t start = clock();
 
+    // Find primes up to that number
+    bool* sieve = find_primes(n);
+
+    // Stop timer (don't want to count printing time)
+    double duration = (clock() - start) / ((double) CLOCKS_PER_SEC);
+
+    cout << endl;
+    if (print_request) {
+        // Print primes
+        cout << "Printing primes up to " << n << ":" << endl;
+        for (int i = 0; i < n; ++i) {
+            if (sieve[i] == false) {
+                // Number is prime
+                cout << i << endl;
+            }
+        }
+    }
+    else if (print_last) {
+        // Print last prime
+        cout << "Last prime: ";
+        for (int i = n; i >= 0; --i) {
+            if (sieve[i] == false) {
+                cout << i << endl;
+                break;
+            }
+        }
+    }
+
+    cout << "time taken: " << duration << "s";
+    cout << " (doesn't include printing time)" << endl;
+}
+
+bool* sieve_of_e(const long long max) {
     // sieve[i] true if i is composite
     // Allocate mem on heap because default stack size
     // is too small at around max = 10,000,000
@@ -68,34 +96,5 @@ void find_primes(const long long max) {
             // all its multiples have been marked off already
         }
     }
-
-    // Stop timer (don't want to count printing time)
-    double duration = (clock() - start) / ((double) CLOCKS_PER_SEC);
-
-    cout << endl;
-    if (print_request) {
-        // Print primes
-        cout << "Printing primes up to " << max << ":" << endl;
-        for (int i = 0; i < max; ++i) {
-            if (sieve[i] == false) {
-                // Number is prime
-                cout << i << endl;
-            }
-        }
-    }
-    else if (print_last) {
-        // Print last prime
-        cout << "Last prime: ";
-        for (int i = max; i >= 0; --i) {
-            if (sieve[i] == false) {
-                cout << i << endl;
-                break;
-            }
-        }
-    }
-
-    cout << "time taken: " << duration << "s";
-    cout << " (doesn't include printing time)" << endl;
-
-    delete[] sieve;
+    return sieve;
 }
